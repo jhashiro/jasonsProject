c8232b869d581e37e53347618fe792c138ca1181
Student 8AM <student8@js-c02mxrrpg083.punahou.edu>
2015-06-26 13:37:12 -1000

    Added Warning alert + extra greeting alerts

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 alert("Hello...Hello...Hello...Hello...Hello...");
+alert("You have acessed a page which radiates an unhealthy amount of coolness. Turn back before you become too cool.");
+alert("Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...Hello...");
 // python -m SimpleHTTPServer 8000
 
